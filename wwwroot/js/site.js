74ce7f14e9bd5572256010b6b818272ee0b7909c
mazzcode@gmail.com <mazzcode@gmail.com>
2022-06-30 00:57:56 -0300

    publicado en la web

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const tl = gsap.timeline({ defaults: { duration: 2 } });
+tl
+    .from('.Testing', { opacity: 0, y: -100 })
+    .from('.texto', { scale: 0, opacity: 0 })
+    .from('.agregar', { opacity: 0, x: -300 })
+    .from('.lista', { x: 300, opacity: 0 }, "-=2")
+    
